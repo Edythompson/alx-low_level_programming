daf3e3469b59430d49dc4f56ca2f62f43d36762f
Edythompson <edyythompson@gmail.com>
2023-07-13 20:25:51 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success) 
+ */
+int main(void)
+{ 
+	Printf('with proper grammar, but the outcomeis a piece of art,\n");
+}
