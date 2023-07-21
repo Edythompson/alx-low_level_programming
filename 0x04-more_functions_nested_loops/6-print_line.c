89ed2deafe7dbdeca814dc69fbb1af24386754f5
Edythompson <edyythompson@gmail.com>
2023-07-21 15:23:56 -0700

    this is done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - Strainght -line
+* @n: No of times
+* Return: Always 0
+*/
+void print_line(int n)
+{
+  int i;
+  for (i = 0; 1 < n; i++)
+  {
+    _putchar('_');
+  }
+  _putchar('\n');
+}
