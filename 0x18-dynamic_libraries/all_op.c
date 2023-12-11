578f535048e0a2b60736e1037234d3b9df3e3086
Edythompson <edyythompson@gmail.com>
2023-12-11 13:09:02 -0800

    operations

---
+++ 0x18-dynamic_libraries/all_op.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+* add - add two numbers
+* @a: first number
+* @b: second number
+* Return: Addition
+*/
+int add(int a, int b)
+{
+return(a + b);
+}
+
+/**
+* sub - subtract two numbers
+* @a: first number
+* @b: second number
+* Return: subtraction of two numbers
+*/
+int sub(int a, int b)
+{
+return(a - b);
+}
+
+/**
+* div - divide two numbers
+* @a: first number
+* @b: second number
+* Return: division
+*/
+int div(int a, int b)
+{
+return(a / b);
+}
+
+/**
+* mod - modulus two numbers
+* @a: first number
+* @b: second number
+* Return: modulus of two numbers
+*/
+int mod(int a, int b)
+{
+return(a % b);
+}
+
+/**
+* mul - multiply two numbers
+* @a: first number
+* @b: second number
+* Return: multiplication
+*/
+int mul(int a, int b)
+{
+return(a * b);
+}
